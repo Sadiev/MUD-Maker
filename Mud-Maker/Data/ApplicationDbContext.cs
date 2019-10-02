3aab141a54080f0eb4c8d09a667ebc39fa6ff608
Wen Lin <wen.l.lin@outlook.com>
2019-10-02 10:12:34 -0400

    Database Design

--- Mud-Maker/Data/ApplicationDbContext.cs
+++ Mud-Maker/Data/ApplicationDbContext.cs
@@ -13,6 +13,40 @@ namespace Mud_Maker.Data
         {
         }
 
+        public DbSet<Mud> Muds { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public class Mud
+        {
+            public int MudId { get; set; }
+            public string MudName { get; set; }
+            public string MudDescription { get; set; }
+        }
+        public class Event
+        {
+            public int EventId { get; set; }
+            public string EventName { get; set; }
+            public string EventDescription { get; set; }
+            public bool EventTriggered { get; set; }
+            public char? DirLeft { get; set; }
+            public char? DirRight { get; set; }
+            public char? DirFwd { get; set; }
 
+
+            public Mud Mud { get; set; }
+            public int MudId { get; set; }
+
+            public EventType EventType { get; set; }
+            public int EventTypeId { get; set; }
+        }
+
+        public class EventType
+        {
+            public int EventTypeId { get; set; }
+
+            public Event Event { get; set; }
+            public int EventId { get; set; }
+
+
+        }
     }
 }
