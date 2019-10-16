c21634e388995b4d0e679625a716ea329043956e
Wen Lin <wen.l.lin@outlook.com>
2019-10-16 08:51:57 -0400

    User Interfaces

---
+++ Mud-Maker/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mud_Maker.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Play()
+        {
+            return View();
+        }
+    }
+}
