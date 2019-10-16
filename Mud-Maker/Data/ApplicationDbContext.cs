a466242917429d23620c37cdb9e7a0a05a460aa1
Seth Way <sethaniel.way@outlook.com>
2019-10-16 09:10:49 -0400

    Initial Seed Data Attempt

--- Mud-Maker/Data/ApplicationDbContext.cs
+++ Mud-Maker/Data/ApplicationDbContext.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Mud_Maker.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
--- Mud-Maker/Models/SeedData.cs
+++ Mud-Maker/Models/SeedData.cs
@@ -2,10 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Mud_Maker.Data;
 
 namespace Mud_Maker.Models
 {
     public static class SeedData
     {
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            ApplicationDbContext context = app.ApplicationServices
+            .GetRequiredService<ApplicationDbContext>();
+            context.Database.Migrate();
+            if (!context.Muds.Any())
+            {
+                context.Muds.AddRange(
+                        new Mud
+                        {
+                            MudName = "Jay's Sweet, Sweet Mud",
+                            MudDescription = "Jay Loves to Make Muds. It is his favorite."
+                        }
+                );
+                context.SaveChanges();
+            }
+        }
     }
 }
