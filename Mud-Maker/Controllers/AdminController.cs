23ca75172203624b599d244aa56be7842217c3c3
Shod <dilshod.sadiev@outlook.com>
2019-10-16 09:05:17 -0400

    Create Mud

--- Mud-Maker/Controllers/AdminController.cs
+++ Mud-Maker/Controllers/AdminController.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Mud_Maker.Data;
+using Mud_Maker.Models;
 
 namespace Mud_Maker.Controllers
 {
     public class AdminController : Controller
     {
+        private ApplicationDbContext repo;
+        public AdminController(ApplicationDbContext repo)
+        {
+            this.repo = repo;
+        }
         public IActionResult Index()
         {
             return View();
@@ -15,6 +22,31 @@ namespace Mud_Maker.Controllers
         public IActionResult CreateMUD()
         {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateMUD(Mud model)
+        {
+            try//if (ModelState.IsValid)
+            {
+                Mud mud = new Mud { MudName=model.MudName, MudDescription=model.MudDescription };
+                repo.Muds.Add(mud);
+                repo.SaveChanges();
+                return RedirectToAction("Index");
+
+                //else
+                //{
+                //    foreach (var error in result.Errors)
+                //    {
+                //        ModelState.AddModelError(string.Empty, error.Description);
+                //    }
+                //}
+
+            }
+            catch
+            {
+                return View(model);
+            }
         }
     }
 }
