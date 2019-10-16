315de2d593efb0076e8ae9799d7a75795044326d
Shod <dilshod.sadiev@outlook.com>
2019-10-16 10:48:57 -0400

    Update DB

--- Mud-Maker/Data/ApplicationDbContext.cs
+++ Mud-Maker/Data/ApplicationDbContext.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Mud_Maker.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
@@ -14,7 +14,6 @@ namespace Mud_Maker.Data
         }
         public DbSet<Mud> Muds { get; set; }
         public DbSet<Event> Events { get; set; }
-        public DbSet<EventType> EventTypes { get; set; }
         public DbSet<Fight> Fights { get; set; }
         public DbSet<Health> HealthBar { get; set; }
         public DbSet<Item> Items { get; set; }
@@ -43,16 +42,6 @@ namespace Mud_Maker.Data
         public Mud Mud { get; set; }
     }
 
-    public class EventType
-    {
-        public int EventTypeId { get; set; }
-        public int FightId { get; set; }
-        public Fight Fight { get; set; }
-        public int HealthId { get; set; }
-        public Health Health { get; set; }
-        public int ItemId { get; set; }
-        public Item Item { get; set; }
-    }
 
     public class Fight
     {
