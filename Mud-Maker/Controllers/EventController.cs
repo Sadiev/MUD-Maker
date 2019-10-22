dd50e1966cf1e34be1de53c7575f47283c07e699
Shod <dilshod.sadiev@outlook.com>
2019-10-22 08:02:36 -0400

    Create event

--- Mud-Maker/Controllers/EventController.cs
+++ Mud-Maker/Controllers/EventController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Mud_Maker.Data;
+using Mud_Maker.Models;
 
 namespace Mud_Maker.Controllers
 {
@@ -20,16 +21,15 @@ namespace Mud_Maker.Controllers
         // GET: Event
         public ActionResult Index(int id)
         {
-            var project = id == 0 ? repo.Events
-                                  : repo.Events.Where(x => x.MudId == id);
-
-            //var viewModel = new ProjectsIndexViewModel { ClientId = id, Projects = project };
+            var events = repo.Events.Where(x => x.MudId == id);
+            var myMud = repo.Muds.Where(x => x.MudId == id).First();
+            var eventViewModel = new EventViewModel { MUD=myMud, Events=events };
             //if (customer == null)
             //{
             //    return NotFound();
             //}
             //Project model = new Project { ProjectID = customer.ProjectID, CompanyName = customer.CompanyName, CompanyPhone = customer.CompanyPhone, CompanyPoc = customer.CompanyPoc };
-            return View();//viewModel
+            return View(eventViewModel);//viewModel
         }
 
         // GET: Event/Details/5
@@ -39,31 +39,47 @@ namespace Mud_Maker.Controllers
         }
 
         // GET: Event/Create
-        public ActionResult Create()
+        public ActionResult Create(int id)
         {
-            return View();
+            return View(new CreateEventViewModel { MUD = id, Fights=repo.Fights, Healths=repo.HealthBar, items=repo.Items });
         }
 
         // POST: Event/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(CreateEventViewModel model)
         {
             try
             {
-                // TODO: Add insert logic here
-
-                return RedirectToAction(nameof(Index));
+                //Project project = new Project { ProjectName = model.ProjectName, StartDate = model.StartDate, DueDate = model.DueDate, TotalTime = DateTime.Now, ClientID = model.ClientID, ProjectManagerID = 1 };
+                repo.Events.Add(new Event {EventName=model.Event.EventName
+                                           ,EventDescription=model.Event.EventDescription
+                                           ,EventText=model.Event.EventText
+                                           ,EventTriggered=model.Event.EventTriggered
+                                           ,DirFwd=model.Event.DirFwd
+                                           ,DirLeft = model.Event.DirLeft
+                                           ,DirRight = model.Event.DirRight
+                                           ,DirBack = model.Event.DirBack
+                                           ,FightId = model.Event.FightId
+                                           ,HealthId = model.Event.HealthId
+                                           ,ItemId = model.Event.ItemId
+                                           ,MudId=model.MUD
+                });
+                repo.SaveChanges();
+                //return RedirectToAction("Index","model.ClientID");
+                //return Index(model.ClientID);
+                return RedirectToAction("Index", new { id = model.MUD });
             }
             catch
             {
-                return View();
+                return View(model);
             }
         }
 
         // GET: Event/Edit/5
         public ActionResult Edit(int id)
-        {
+        {           
+            
             return View();
         }
 
